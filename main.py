52ad00bf722a57906f76a93d5a509cf1f07791a0
Baret Buckley <bbuckley@FED-DT02.pro.evrt.derbylabs.net>
2019-11-30 20:00:49 -0500

    added all walls

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 #!/bin/env python3
-walls = {0:" ", 1:"│", 2:"─"}
+walls = {0:" ", 1:"│", 2:"─", 3:"┘", 4:"┐", 5:"┌", 6:"└", 7:"┴", 8:"┤", 9:"┬", 10:"├", 11:"┼"}
 
 def new_map(h, w):
-    map = [[(1, 1) for rj in range(w)] for j in range(h)]
+    map = [[(0, 1) for rj in range(w)] for j in range(h)]
     return map
 
 def generate(map):
     for j in range (len(map[0])):
         map[0][j] = (2, 1)
+        map[len(map)-1][j] = (2, 1)
+    for j in range (len(map)):
+        map[j][0] = (1, 1)
+        map[j][len(map[0])-1] = (1, 1)
+    map[0][0] = (5, 1)
+    map[len(map)-1][0] = (6, 1)
+    map[0][len(map[0])-1] = (4, 1)
+    map[len(map)-1][len(map[0])-1] = (3, 1)
     return map
 
 def print_dungeon(dungeon):
